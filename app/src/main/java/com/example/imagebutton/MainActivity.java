2ab44c1c685e6629438949b188c03c1c35857c5f
liem <lb6031@bs.amalnet.k12.il>
2019-01-02 23:14:20 +0200

    finish

--- app/src/main/java/com/example/imagebutton/MainActivity.java
+++ app/src/main/java/com/example/imagebutton/MainActivity.java
@@ -9,34 +9,32 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgv;
-    ImageButton imgb;
+    Random rd=new Random();
+    ImageView iv;
+    ImageButton ib;
     int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imgv=(ImageView) findViewById(R.id.imgv);
-        imgb=(ImageButton) findViewById(R.id.imgb);
+        iv = (ImageView) findViewById(R.id.iv);
+        ib = (ImageButton) findViewById(R.id.ib);
     }
 
-    public void click(View view) {
-        Random rd= new Random();
+    public void clicked(View view) {
         x=rd.nextInt(3)+1;
-        if (x==1){
-            imgv.setImageResource(R.drawable.picone);
-            imgb.setImageResource(R.drawable.one);
-        }
-        else{
-            if (x==2) {
-                imgv.setImageResource(R.drawable.pictwo);
-                imgb.setImageResource(R.drawable.two);
-            }
-            else {
-                imgv.setImageResource(R.drawable.picthree);
-                imgb.setImageResource(R.drawable.three);
-            }
+        switch (x){
+            case 1: iv.setImageResource(R.drawable.picone);
+                    ib.setImageResource(R.drawable.one);
+                    break;
+            case 2: iv.setImageResource(R.drawable.pictwo);
+                    ib.setImageResource(R.drawable.two);
+                    break;
+            case 3: iv.setImageResource(R.drawable.picthree);
+                    ib.setImageResource(R.drawable.three);
+                    break;
+
         }
     }
 }
